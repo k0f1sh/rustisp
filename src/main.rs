cce9db135fc9e28b208f82f0d785ce523b26ace0
k0f1sh <2574472+k0f1sh@users.noreply.github.com>
2024-08-11 22:23:32 +0900

    add repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io::Write;
+
 #[macro_use]
 mod sexp;
 
@@ -7,5 +9,22 @@ mod token;
 mod parser;
 
 fn main() {
-    println!("Hello, world!");
+    println!("I'm Rustisp. Your code is BAD");
+
+    // repl
+    loop {
+        print!("> ");
+        std::io::stdout().flush().unwrap();
+        let mut input = String::new();
+        std::io::stdin().read_line(&mut input).unwrap();
+        let input = input.trim();
+        if input.is_empty() {
+            continue;
+        }
+        let tokens = lexer::lex(input);
+        let sexps = parser::parse(tokens).unwrap();
+        for sexp in sexps {
+            println!("{}", sexp);
+        }
+    }
 }
